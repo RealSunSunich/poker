de786bd07429097613d2d90595fa71bb4c49f2e2
SunSunich <sunsunich@gmail.com>
2016-05-24 14:22:09 +0300

    Small arc changes

--- poker/src/poker/Main.java
+++ poker/src/poker/Main.java
@@ -13,6 +13,8 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -25,31 +27,33 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+class Player{
+	public String ip;
+	public int num;
+	int[] cards=new int[2];
+	int counter=0;
+
+	public Player(String ip, int num) {
+		this.ip = ip;
+		// TODO Auto-generated constructor stub
+		this.num = num;
+	}
+
+	public void giveCards(int i) {
+		System.out.println(i);
+		
+		if(this.counter>=2)
+			return;
+		this.cards[this.counter]=i;
+		this.counter++;
+		
+	}
+}
+
+
 public class Main extends Application{
 	
-	class Player{
-		public String ip;
-		public int num;
-		private int[] cards=new int[2];
-		private int counter=0;
-
-		public Player(String ip, int num) {
-			this.ip = ip;
-			// TODO Auto-generated constructor stub
-			this.num = num;
-		}
-
-		public void giveCards(int i) {
-			System.out.println(i);
-			
-			if(this.counter>=2)
-				return;
-			this.cards[this.counter]=i;
-			this.counter++;
-			
-		}
-	}
-	
+		
 	static class Options{
 		@Parameter(names = "-server", description = "Default - server mode, false if client", arity = 1)
 		static boolean type = true;	
@@ -94,7 +98,15 @@ public class Main extends Application{
 
 	}
 
-	private int counter;
+	private static int counter;
+	private static FlowPane pane;
+	private static Scene ms;
+	private static volatile GraphicsContext gc;
+	private static volatile Stage primaryStage;
+	private static Image img;
+	private static int[] cardsIdxs = new int[52];
+	private static List<Player> players;
+	private static int cardHead;;
 
 	private static void swap(int[] array, int i, int j) {
         int temp = array[i];
@@ -104,17 +116,16 @@ public class Main extends Application{
 	
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage stage) throws Exception {
 		
+		primaryStage = stage;
 		System.out.println(Options.type);
 		
 		
 		
-		int[] cardsIdx = new int[52];
 		
-		
-		for(int i=0; i<=cardsIdx.length-1;i++){
-			cardsIdx[i]=i;
+		for(int i=0; i<=cardsIdxs.length-1;i++){
+			cardsIdxs[i]=i;
 		}
 	
 		
@@ -122,26 +133,25 @@ public class Main extends Application{
 		
 		Random random = new Random();
 		
-		for (int i = cardsIdx.length; i > 1; i--) {
-            swap(cardsIdx, i - 1, random.nextInt(i));
+		for (int i = cardsIdxs.length; i > 1; i--) {
+            swap(cardsIdxs, i - 1, random.nextInt(i));
         }
 		
 		File cardsImgFile = new File("res/cards.jpg");
 		
-		Image img = new Image(new FileInputStream(cardsImgFile));
-//		img.getPixelReader().
+		img = new Image(new FileInputStream(cardsImgFile));
 		
 				
 		primaryStage.setTitle("Best Super Mega Poker");
 		
-		FlowPane pane = new FlowPane();
+		pane = new FlowPane();
 		
-		Scene ms = new Scene(pane,1024,768);
+		ms = new Scene(pane,1024,768);
 		
 		primaryStage.setScene(ms);
 		
 		Canvas cnvs = new Canvas(800,600);
-		GraphicsContext gc = cnvs.getGraphicsContext2D();
+		gc = cnvs.getGraphicsContext2D();
 		Button check = new Button("Check");
 		Button call = new Button("Call");
 		Button raise = new Button("Raise");
@@ -155,138 +165,27 @@ public class Main extends Application{
 		
 		
 		
-		List<Player> players = new ArrayList<>(3);
-//		players.add(0);
-//		players.add(1);
-//		players.add(2);
+		players = new ArrayList<>(3);
 		
 		CycleIterator<Object> player = new CycleIterator<Object>(players);
 		
 		if(Options.type){
 			//Create Server
 			
-			ServerSocketChannel serverChannel;
-			Selector selector;
-			try {
-				serverChannel = ServerSocketChannel.open();
-//				ServerSocket ss = serverChannel.socket();
-				InetSocketAddress address = new InetSocketAddress(777);
-				serverChannel.bind(address);
-				serverChannel.configureBlocking(false);
-				selector = Selector.open();
-				serverChannel.register(selector, SelectionKey.OP_ACCEPT);
-			} catch (IOException ex) {
-				ex.printStackTrace();
-				return;
-			}
-			
-			int cardHead=0;
-			drawCards(img, gc, cardsIdx[cardHead++], 0);
-			drawCards(img, gc, cardsIdx[cardHead++], 0);
-			primaryStage.show();
-			
-			boolean done= true;
-			while(done){
-				try {
-					selector.select();
-				} catch (IOException ex) {
-					ex.printStackTrace();
-					break;
-				}
-
-				Set<SelectionKey> readyKeys = selector.selectedKeys();
-				Iterator<SelectionKey> iterator = readyKeys.iterator();
-				Integer playerNum=0;
+			Runnable r = new Runnable() {
 				
-				while (iterator.hasNext()) {
-					
-					SelectionKey key = iterator.next();
-					iterator.remove();
-					try {
-						if (key.isAcceptable()) {
-							playerNum++;
-							
-														
-							ServerSocketChannel server = (ServerSocketChannel) key.channel();
-							SocketChannel client = server.accept();
-							
-							Player e = new Player(client.getLocalAddress().toString(),playerNum);
-							
+				
 
-							
-							System.out.println("Accepted connection from " + client);
-							client.configureBlocking(false);
-							SelectionKey key2 = client.register(selector, SelectionKey.OP_WRITE);
-							
-							key2.attach(e);
-							
-							
-							
-						} else if (key.isWritable()&& ((Player)key.attachment()).counter==0) {
-							SocketChannel client = (SocketChannel) key.channel();
-							ByteBuffer buffer = ByteBuffer.allocate(72);
-							
-							Player e = (Player)key.attachment();
-							
-							if(!players.contains(e)){
-								players.add(e);
-								buffer.putInt(e.num);
-								buffer.flip();
-								client.write(buffer);
-								buffer.clear();
-							}
-							
-							
-							
-							e.giveCards(cardsIdx[cardHead++]);
-							e.giveCards(cardsIdx[cardHead++]);
-							
-							buffer.putInt(e.cards[0]);
-							buffer.putInt(e.cards[1]);
-							
-							
-							
-							
-							
-							/*//fill buffer with data
-							for(; cardHead<=(2*players.size()-1); cardHead++){
-								int nextInt = cardsIdx[cardHead];
-								System.out.println(nextInt);
-
-								Player next = player.next();
-								System.out.println(next.ip);
-								System.out.println(client.getLocalAddress().toString());
-								if(client.getLocalAddress().toString().equals(next.ip))
-									buffer.putInt(nextInt);
-
-								drawCards(img, gc, nextInt, next.num);
-
-								if(cardHead%players.size()==0){
-									counter++;
-								}
-
-								primaryStage.show();
-								
-							}*/
-							buffer.flip();
-							client.write(buffer);
-//							key.cancel();
-//							break;
-							done = false;
-							
-						}
-					} catch (IOException ex) {
-						key.cancel();
-						try {
-							key.channel().close();
-						}
-						catch (IOException cex) {}
-					}
+				@Override
+				public void run() {
+					Main.createServer();	
+//					return null;
 				}
 
-			}
+			};
 			
-			
+			Thread thread = new Thread(r, "Poker Server");
+			thread.start();
 			
 		}
 		else{
@@ -356,8 +255,109 @@ public class Main extends Application{
 */		
 		
 	}
+	
+	static void createServer(){
+		ServerSocketChannel serverChannel;
+		Selector selector;
+		try {
+			serverChannel = ServerSocketChannel.open();
+			InetSocketAddress address = new InetSocketAddress(777);
+			serverChannel.bind(address);
+			serverChannel.configureBlocking(false);
+			selector = Selector.open();
+			serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return;
+		}
+		cardHead = 0;
+		
+		Platform.runLater(()->{
+			drawCards(img, gc, cardsIdxs[cardHead++], 0);
+			drawCards(img, gc, cardsIdxs[cardHead++], 0);
+			primaryStage.show();
+			
+		});
+		
+		boolean done= true;
+		while(done){
+			try {
+				selector.select();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+				break;
+			}
 
-	private void drawCards(Image img, GraphicsContext gc, int nextInt, Integer player) {
+			Set<SelectionKey> readyKeys = selector.selectedKeys();
+			Iterator<SelectionKey> iterator = readyKeys.iterator();
+			Integer playerNum=0;
+			
+			while (iterator.hasNext()) {
+				
+				SelectionKey key = iterator.next();
+				iterator.remove();
+				try {
+					if (key.isAcceptable()) {
+						playerNum++;
+						
+													
+						ServerSocketChannel server = (ServerSocketChannel) key.channel();
+						SocketChannel client = server.accept();
+						
+						Player e = new Player(client.getLocalAddress().toString(),playerNum);
+						
+
+						
+						System.out.println("Accepted connection from " + client);
+						client.configureBlocking(false);
+						SelectionKey key2 = client.register(selector, SelectionKey.OP_WRITE);
+						
+						key2.attach(e);
+						
+						
+						
+					} else if (key.isWritable()&& ((Player)key.attachment()).counter==0) {
+						SocketChannel client = (SocketChannel) key.channel();
+						ByteBuffer buffer = ByteBuffer.allocate(72);
+						
+						Player e = (Player)key.attachment();
+						
+						if(!players.contains(e)){
+							players.add(e);
+							buffer.putInt(e.num);
+							buffer.flip();
+							client.write(buffer);
+							buffer.clear();
+						}
+						
+						e.giveCards(cardsIdxs[cardHead++]);
+						e.giveCards(cardsIdxs[cardHead++]);
+						
+						buffer.putInt(e.cards[0]);
+						buffer.putInt(e.cards[1]);
+						
+						buffer.flip();
+						client.write(buffer);
+						
+						
+						
+//						done = false;
+						
+					}
+				} catch (IOException ex) {
+					key.cancel();
+					try {
+						key.channel().close();
+					}
+					catch (IOException cex) {}
+				}
+			}
+
+		}
+		
+	}
+
+	private static void drawCards(Image img, GraphicsContext gc, int nextInt, Integer player) {
 		System.out.println("drawing " + nextInt + " card");
 		if(nextInt<13){
 //			gc.drawImage(img, 215, 300, 120,180, 0, nextInt*180, 120, 180);
@@ -378,7 +378,7 @@ public class Main extends Application{
 		counter++;
 	}
 
-	private void drawCard(Image img, GraphicsContext gc, int column, int nextInt, int player) {
+	private static void drawCard(Image img, GraphicsContext gc, int column, int nextInt, int player) {
 		if (player==0)
 			gc.drawImage(img, 
 				120*column, (nextInt)*180, 120, 180
